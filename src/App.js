58f003fabba4737d4c253cc109dc832eeece9aba
oat <bossun_fujisaki2000@hotmail.com>
2022-08-11 17:45:47 +0700

    part 4

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import WordCard from './WordCard';
 
+const word = "Hello";
 function App() {
   return (
     <div>
-      
+      <WordCard value={word}/>
     </div>
   );
 }
